569be0393cf896e9e11ce9330e130cbd0000b130
Hrant-p <hrant@live.ru>
2019-07-20 15:49:35 +0400

    set posts

---
+++ src/components/Posts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Posts = ({ posts, loading }) => {
+    if (loading) {
+        return <h2>Loading ...</h2>
+    };
+
+
+    return (
+        <ul className="list-group mb-4">
+            {posts.map(post => (
+                <li
+                    className="list-item"
+                    key={post.id}>
+                    {post.title}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default Posts;
